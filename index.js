893ac6c27ae096a000bdf5da99a959a03c8a7ab0
naimurrahmanniloy <niloyrahman432@gmail.com>
2022-11-30 01:26:47 +0600

    done with the catagory area

--- index.js
+++ index.js
@@ -13,11 +13,25 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.oah4mk6.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        const categoryCollection = client.db('phoneStore').collection('category');
+
+        app.get('/category', async (req, res) => {
+            const query = {}
+            const result = categoryCollection.find(query);
+            const category = await result.toArray();
+            res.send(category)
+        })
+    }
+
+    finally {
+
+    }
+}
+
+run().catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
