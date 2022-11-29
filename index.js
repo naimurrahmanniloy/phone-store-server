4f68a2ada9c5c029f71e949e25c74a9d31942d2f
naimurrahmanniloy <niloyrahman432@gmail.com>
2022-11-30 00:33:33 +0600

    done with the first init

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const port = process.env.PORT || 5000;
 
-//middleware
+//middlewares
 app.use(cors());
 app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Phone store is running')
+})
+
+app.listenerCount(port, () => {
+    console.log(`Phone Store server is running on ${port}`)
+})
