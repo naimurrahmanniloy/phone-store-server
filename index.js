f647f28b8ffccd9bea3bd0cd972dfca8972c6114
naimurrahmanniloy <niloyrahman432@gmail.com>
2022-11-30 00:46:09 +0600

    integrating the mongoDB

--- index.js
+++ index.js
@@ -2,15 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-//middlewares
+//middle wares
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.oah4mk6.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
 
 app.get('/', (req, res) => {
     res.send('Phone store is running')
 })
 
-app.listenerCount(port, () => {
+app.listen(port, () => {
     console.log(`Phone Store server is running on ${port}`)
 })
