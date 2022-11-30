bf95b451d736e596a6db48b17e4ad3e5d74df949
naimurrahmanniloy <niloyrahman432@gmail.com>
2022-12-01 00:26:24 +0600

    Done with the posting booking

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ async function run() {
     try {
         const categoryCollection = client.db('phoneStore').collection('category');
         const usersCollection = client.db('phoneStore').collection('users');
+        const sellPostCollection = client.db('phoneStore').collection('sellpost');
+        const allPhoneCollection = client.db('phoneStore').collection('allPhones');
+        const bookingCollection = client.db('phoneStore').collection('bookings');
+
 
         app.get('/category', async (req, res) => {
             const query = {}
@@ -26,7 +30,42 @@ async function run() {
             res.send(category)
         });
 
-        //for user query
+        app.get('/allPhones', async (req, res) => {
+            const query = {}
+            const phone = allPhoneCollection.find(query);
+            const result = await phone.toArray();
+            res.send(result)
+        })
+
+        app.get('/allPhones/:id', async (req, res) => {
+            const id = req.params.category;
+            console.log(id)
+            const query = { category: id }
+            const phone = await allPhoneCollection.findOne(query);
+            res.send(phone)
+        })
+        app.get('/category/:category', async (req, res) => {
+            const id = req.params.category;
+            const query = { category: id };
+            const category = await allPhoneCollection.find(query).toArray();
+            res.send(category)
+        });
+        //Posting the information for booking
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        });
+
+        //admin 
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+        });
+
+        //for user 
         app.get('/users', async (req, res) => {
             const query = {};
             const users = await usersCollection.find(query).toArray();
@@ -39,6 +78,8 @@ async function run() {
             const result = await usersCollection.insertOne(user);
             res.send(result);
         });
+
+
     }
 
     finally {
